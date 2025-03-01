edf016a8a5b93b7fdf7d56a9a138ae69e01a2224
pyschonoice <monukk2002@gmail.com>
2025-03-01 20:59:52 +0530

    fixed port to 4000

--- server/server.js
+++ server/server.js
@@ -251,7 +251,7 @@ app.get('/subtitles/:fileId/:index', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 }); 
